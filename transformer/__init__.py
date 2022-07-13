3b86771e7f0551d5a921672b4101b952f6a36431
0x404 <871206929@qq.com>
2022-07-13 22:55:45 +0800

    mk: make it a package

---
+++ transformer/__init__.py
@@ -0,0 +1,3 @@
+from .transformer import (Transformer, TransformerEncoder, TransformerEncoderBlock, TransformerDecoder, TransformerDecoderBlock)
+
+__all__ = ["Transformer", "TransformerEncoder", "TransformerEncoderBlock", "TransformerDecoder", "TransformerDecoderBlock"]
